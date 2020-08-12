2a890ca099573e66692cbca69ffd3b43eec13834
Neil <643241432@qq.com>
2020-08-12 11:00:07 +0800

    更新了VC和MainStoryBoard

---
+++ 皮影之路/MenuPage.swift
@@ -0,0 +1,30 @@
+//
+//  Menu.swift
+//  皮影之路
+//
+//  Created by 张雨飞 on 12/08/20.
+//  Copyright © 2020 张雨飞. All rights reserved.
+//
+
+import UIKit
+
+class Menu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
